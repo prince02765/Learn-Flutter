926ff938d83f26c00d59539142721102f1a0ab6b
Prince Patel <princepatel221350@gmail.com>
2021-10-01 06:36:16 +0530

    day-13

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,10 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:futter_learn/Models/catalog.dart';
 import 'package:futter_learn/Widgets/drawer.dart';
 import 'package:futter_learn/Widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodecData = jsonDecode(catalogJson);
+    var productsData = decodecData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
