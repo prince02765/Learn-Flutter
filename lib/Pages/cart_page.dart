01ff187ec606657b528de7371ea74df302eaf25b
Prince Patel <princepatel221350@gmail.com>
2021-10-07 08:09:58 +0530

    day-22

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:futter_learn/Widgets/themes.dart';
 import 'package:velocity_x/src/extensions/context_ext.dart';
 import 'package:velocity_x/src/extensions/string_ext.dart';
+import 'package:velocity_x/src/flutter/padding.dart';
+import 'package:velocity_x/src/flutter/widgets.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -13,6 +16,61 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
+      ),
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+
+class _CartTotal extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl4.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: (){}, 
+            child: "Buy".text.xl.make(),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(context.theme.buttonColor),
+            ),
+          ).w24(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: "Item 1".text.make(),
       ),
     );
   }
